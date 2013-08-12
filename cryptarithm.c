1329d6d16e122d315d305192167eb97cffe33360
haohaolin1988 <haohaolin1988@gmail.com>
2013-08-12 22:30:10 +1000

    Deleted excessive looping

--- cryptarithm.c
+++ cryptarithm.c
@@ -24,9 +24,9 @@
 /*The least possible value of organce is 102345 and max is 987654*/
 #define MIN_ORG 102345
 #define MAX_ORG 987654
-/*The least possible value of grape is 10234 and max is 98765*/
-#define MIN_GRP 10234
-#define MAX_GRP 98765
+/*The least possible value of p is 1 and max is 9*/
+#define MIN_P 1
+#define MAX_P 9
 
                        
 int main(void) {
@@ -49,24 +49,27 @@ int main(void) {
         int e = i % 10;
         if (++digits_org[e] == 2)
             continue;
-        for (int j = MIN_GRP; j <= MAX_GRP; ++j) {
-            int digits_org_grp[10];
-            for (int ja = 0; ja < 10; ++ja)
-                digits_org_grp[ja] = digits_org[ja]; 
-            int p = j / 10 % 10;
-            if (p == 0 || ++digits_org_grp[p] == 2)
+        for (int j = MIN_P; j <= MAX_P; ++j) {
+            if (digits_org[j] + 1 == 2)
                 continue;
-            int b = ((o * 100000 + r * 10000 + a * 1000 + n * 100 + g * 10 + e) - (g * 10000 + r * 1000 + a * 100 + p * 10 + e) - (p * 1000 + e * 100 + a * 10 + r)) / 10000;
-            if (b == 0 || ++digits_org_grp[b] == 2) 
+            int p = j;
+            digits_org[p] = 1;
+            int b = ((o * 100000 + r * 10000 + a * 1000 + n * 100 + g * 10 + e) - (g * 10000 + r * 1000 + a * 100 + p * 10 + e) - (p * 1000 + e * 100 + a * 10 + r)) / 100000;
+            if (b == 0 || digits_org[b] + 1 == 2) { 
+                digits_org[p] = 0;
                 continue;
-            else {
-                int orange = o * 100000 + r * 10000 + a * 1000 + n * 100 + g * 10 + e;
-                int grape = g * 10000 + r * 1000 + a * 100 + p * 10 + e;
-                int pear = p * 1000 + e * 100 + a * 10 + r;
-                int banana = b * 100000 + a * 10000 + n * 1000 + a * 100 + n * 10 + a;
-                printf("banana = %d, grape = %d, pear = %d and orange = %d is a solution", banana, grape, pear, orange);
             }
+            int orange = o * 100000 + r * 10000 + a * 1000 + n * 100 + g * 10 + e;
+            int grape = g * 10000 + r * 1000 + a * 100 + p * 10 + e;
+            int pear = p * 1000 + e * 100 + a * 10 + r;
+            int banana = b * 100000 + a * 10000 + n * 1000 + a * 100 + n * 10 + a;
+            if (banana + pear + grape == orange) {
+                printf("banana = %d, grape = %d, pear = %d and orange = %d is a solution.", banana, grape, pear, orange);
+                return EXIT_SUCCESS;
+            }
+            digits_org[p] = 0;
         }
     }
+    return EXIT_SUCCESS;
 }
 
